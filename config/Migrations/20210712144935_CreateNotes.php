3109eb29e8aa752b2ec6d6f8d3819d2cecc1b8bf
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-12 11:51:39 -0300

    bin/cake bake migration CreateNotes title note created modified | bin/cake migrations migrate

---
+++ config/Migrations/20210712144935_CreateNotes.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateNotes extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('notes');
+        $table->addColumn('title', 'string', [
+            'default' => null,
+            'limit' => 50,
+            'null' => false,
+        ]);
+        $table->addColumn('note', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}

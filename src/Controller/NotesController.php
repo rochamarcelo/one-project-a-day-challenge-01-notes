f9e1f3cb965ed6402358aeb548bc8054df15d46b
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-12 13:22:03 -0300

    Remove unused files

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -40,66 +40,6 @@ class NotesController extends AppController
     }
 
     /**
-     * View method
-     *
-     * @param string|null $id Note id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $note = $this->Notes->get($id, [
-            'contain' => [],
-        ]);
-
-        $this->set(compact('note'));
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $note = $this->Notes->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $note = $this->Notes->patchEntity($note, $this->request->getData());
-            if ($this->Notes->save($note)) {
-                $this->Flash->success(__('The note has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The note could not be saved. Please, try again.'));
-        }
-        $this->set(compact('note'));
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Note id.
-     * @return \Cake\Http\Response|null|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $note = $this->Notes->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $note = $this->Notes->patchEntity($note, $this->request->getData());
-            if ($this->Notes->save($note)) {
-                $this->Flash->success(__('The note has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The note could not be saved. Please, try again.'));
-        }
-        $this->set(compact('note'));
-    }
-
-    /**
      * Delete method
      *
      * @param string|null $id Note id.

ac24ecd22fa1bca1dd957a4caa5354cf1dedca33
Marcelo Rocha <contato@omarcelo.com.br>
2021-07-12 12:10:18 -0300

    Improve index css with bootstrap

--- templates/Notes/index.php
+++ templates/Notes/index.php
@@ -5,45 +5,37 @@
  */
 ?>
 <div class="notes index content">
-    <?= $this->Html->link(__('New Note'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Notes') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('title') ?></th>
-                    <th><?= $this->Paginator->sort('note') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($notes as $note): ?>
-                <tr>
-                    <td><?= $this->Number->format($note->id) ?></td>
-                    <td><?= h($note->title) ?></td>
-                    <td><?= h($note->note) ?></td>
-                    <td><?= h($note->created) ?></td>
-                    <td><?= h($note->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $note->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $note->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $note->id], ['confirm' => __('Are you sure you want to delete # {0}?', $note->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
+    <?= $this->Html->link(
+        __('New Note'),
+        ['action' => 'add'],
+        ['class' => 'btn btn-primary mb-3']
+    ) ?>
+    <?php foreach ($notes as $note): ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title"><?= h($note->title) ?></h5>
+                <h6 class="card-subtitle mb-2 text-muted"><?= h($note->modified) ?></h6>
+                <p class="card-text"><?= h($note->note) ?></p>
+                <?= $this->Html->link(
+                    __('Edit'),
+                    ['action' => 'edit', $note->id]
+                ) ?>
+                <?= $this->Form->postLink(
+                    __('Delete'),
+                    [
+                        'action' => 'delete',
+                        $note->id
+                    ],
+                    ['confirm' => __('Are you sure you want to delete # {0}?', $note->id)]
+                ) ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
     <div class="paginator">
         <ul class="pagination">
             <?= $this->Paginator->first('<< ' . __('first')) ?>
             <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
         </ul>
         <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
     </div>
